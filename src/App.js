29eb92dbcde58b4bba7279b98d562ce4d3570975
Kai Takami <kaitakami@protonmail.com>
2022-08-26 22:01:39 +0900

    Add styles to form inputs and labels

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { Component } from "react";
 import Form from "./components/Form";
 import Result from "./components/Result";
 
-
 class App extends Component {
   constructor(props) {
     super();
@@ -10,10 +9,18 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <div className="container md:grid md:grid-cols-2">
-          <Form />
-          <Result />
+      <div className="App min-h-screen bg-slate-100">
+        <div className="container">
+          <h1 className="font-bold text-3xl md:text-5xl text-center p-4">
+            CV Generator
+          </h1>
+          <p className="font-bold text-center pl-4 mb-5">
+            Create a professional CV in 5 minutes by just filling this form
+          </p>
+          <div className="container md:grid md:grid-cols-2 m-auto py-6 gap-7">
+            <Form />
+            <Result />
+          </div>
         </div>
       </div>
     );
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,95 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-export default class Form extends Component {
+class Form extends Component {
+  constructor(props) {
+    super()
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('heyyy')
+  }
+
   render() {
     return (
-      <form>
-        <div className="field">Field</div>
+      <form className="container">
+        <div className="double-field gap-6 grid grid-cols-2">
+          <div className="flex flex-col w-full">
+            <label
+              className="font-bold mt-5 text-gray-500 text-lg"
+              htmlFor="name"
+            >
+              Name:
+            </label>
+            <input
+              type="text"
+              id="name"
+              className="border border-slate-300 rounded-sm md:rounded-md px-2 py-2 mt-2"
+              placeholder="Your First Name"
+            />
+          </div>
+          <div className="flex flex-col w-full">
+            <label
+              className="font-bold mt-5 text-gray-500 text-lg"
+              htmlFor="surname"
+            >
+              Surname:
+            </label>
+            <input
+              type="text"
+              id="surname"
+              className="border border-slate-300 rounded-sm md:rounded-md px-2 py-2 mt-2"
+              placeholder="Your Last Name"
+            />
+          </div>
+        </div>
+        <div className="flex flex-col w-full">
+          <label
+            className="font-bold mt-5 text-gray-500 text-lg"
+            htmlFor="phone"
+          >
+            Phone:
+          </label>
+          <input
+            type="text"
+            pattern=""
+            id="phone"
+            className="border border-slate-300 rounded-sm md:rounded-md px-2 py-2 mt-2 invalid:border-red-300 valid:border-green-300 outline-none"
+            placeholder="ex. +81 473 118 8692"
+          />
+        </div>
+        <div className="flex flex-col w-full">
+          <label
+            className="font-bold mt-5 text-gray-500 text-lg"
+            htmlFor="name"
+          >
+            Email:
+          </label>
+          <input
+            type="email"
+            id="name"
+            className="border border-slate-300 rounded-sm md:rounded-md px-2 py-2 mt-2"
+            placeholder="your@email.com"
+          />
+        </div>
+        <div className="flex flex-col w-full">
+          <label
+            className="font-bold mt-5 text-gray-500 text-lg"
+            htmlFor="website"
+          >
+            Website: <span className="text-sm">(optional)</span>
+          </label>
+          <input
+            type="url"
+            id="website"
+            className="border border-slate-300 rounded-sm md:rounded-md px-2 py-2 mt-2"
+            placeholder="https://yourwebsite.com"
+          />
+        </div>
+        <input type="submit" value="Download PDF" className="bg-blue-400 py-5 px-3 rounded-md text-white border-blue-300 font-bold mt-7 cursor-pointer" onClick={(e) => this.handleSubmit(e)} />
       </form>
-    )
+    );
   }
 }
+
+export default Form;
